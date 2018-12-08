53b368a6106fc60dcfbeccac6daa216c6fcceb62
sergio-VM_16_04_3-32bits <sergigon@ing.uc3m.es>
2018-12-08 20:14:25 +0100

    Improved requests lists

--- include/time_weather_skill/weather_apixu.py
+++ include/time_weather_skill/weather_apixu.py
@@ -31,8 +31,12 @@ class Apixu():
 	# URLs
 	url_current = 'http://api.apixu.com/v1/current.json' # URL para current weather
 	url_forecast = 'http://api.apixu.com/v1/forecast.json' # URL para forecast weather
-
 	url_icon = 'http://www.apixu.com/doc/Apixu_weather_conditions.json' # URL para informacion de los iconos
+
+	# Lists for the info required
+	info_basic_list = ['last_updated', 'city_name', 'country_name', 'date', 'avg_temp_c', 'text', 'code'] # Basic list
+	info_advanced_list = info_basic_list[:]
+	info_advanced_list.extend(['mintemp_c', 'maxtemp_c', 'totalprecip_mm']) # Advanced list
 
 	def __init__(self):
 		"""
@@ -43,7 +47,7 @@ class Apixu():
 		self.__info = '' # Weather info
 		self.__city_name = ''
 		self.__result = -1
-		self.__result_info = ''
+		self.__result_info = {}
 
 	def _request(self, location=location_def, days=apixu_limit_weather, lang='es', key=api_key_serg):
 		"""
@@ -71,8 +75,11 @@ class Apixu():
 		#                        #
 		##########################
 
-		print(type(self.__data))
-		#print(self.__data)
+	def _update(self):
+		"""
+		Looks if there is already weather info in local.
+		Used to not make unnecessary requests.
+		"""
 
 	def _get_info(self, date, info_required):
 		"""
@@ -82,25 +89,55 @@ class Apixu():
 		@param imfo_required: 
 		"""
 
-		date = int(date) # Converts to intthe date for the dictionary
+		# Converts to int the date for the dictionary
+		date = int(date)
+
+		# Prepare info_required list
+		info_required = info_required.replace(' ','') # Remove spaces form the string
+		info_required_list = info_required.split(",") # Separate the goal in various parts
+
+		if('advanced' in info_required_list): # Check if advanced is requested
+			info_required_list = self.info_advanced_list[:] # Replace the list with the advanced list
+
+		if('basic' in info_required_list): # Check if basic is requested
+			info_required_list = self.info_basic_list[:] # Replace the list with the basic list
+		
 		# Forecast
-		if (info_required == 'basic' or info_required == 'advanced'):
-			self.__result_info = {
-				'last_updated': self.__data['current']['last_updated'], # last updated
-				'city name': self.__data['location']['name'], # city name
-				'country_name': self.__data['location']['country'], # country name
-				'date': self.__data['forecast']['forecastday'][date]['date'], # date
-				'avg_temp_c': self.__data['forecast']['forecastday'][date]['day']['avgtemp_c'], # avgtemp_c
-				'text': self.__data['forecast']['forecastday'][date]['day']['condition']['text'], # text
-				'code': self.__data['forecast']['forecastday'][date]['day']['condition']['code'] # code
-			}
-			if (info_required == 'advanced'):
-				self.__result_info.update({
-					'mintemp_c': self.__data['forecast']['forecastday'][date]['day']['mintemp_c'], # mintemp_c
-					'maxtemp_c': self.__data['forecast']['forecastday'][date]['day']['maxtemp_c'], # maxtemp_c
-					'totalprecip_mm': self.__data['forecast']['forecastday'][date]['day']['totalprecip_mm'] # totalprecip_mm
-					})
+		for info_required_i in info_required_list:
+
+			if (info_required_i == 'last_updated'): # last updated
+				self.__result_info.update({'last_updated': self.__data['current']['last_updated']})
+
+			if (info_required_i == 'city_name'): # city name
+				self.__result_info.update({'city_name': self.__data['location']['name']})
+
+			if (info_required_i == 'country_name'): # country name
+				self.__result_info.update({'country_name': self.__data['location']['country']})
+
+			if (info_required_i == 'date'): # date
+				self.__result_info.update({'date': self.__data['forecast']['forecastday'][date]['date']})
+
+			if (info_required_i == 'avg_temp_c'): # avg_temp_c
+				self.__result_info.update({'avg_temp_c': self.__data['forecast']['forecastday'][date]['day']['avgtemp_c']})
+
+			if (info_required_i == 'text'): # text
+				self.__result_info.update({'text': self.__data['forecast']['forecastday'][date]['day']['condition']['text']})
+
+			if (info_required_i == 'code'): # code
+				self.__result_info.update({'code': self.__data['forecast']['forecastday'][date]['day']['condition']['code']})
+
+			if (info_required_i == 'mintemp_c'): # mintemp_c
+				self.__result_info.update({'mintemp_c': self.__data['forecast']['forecastday'][date]['day']['mintemp_c']})
+
+			if (info_required_i == 'maxtemp_c'): # maxtemp_c
+				self.__result_info.update({'maxtemp_c': self.__data['forecast']['forecastday'][date]['day']['maxtemp_c']})
+
+			if (info_required_i == 'totalprecip_mm'): # totalprecip_mm
+				self.__result_info.update({'totalprecip_mm': self.__data['forecast']['forecastday'][date]['day']['totalprecip_mm']})
+
+		# Print of the result info
 		print(self.__result_info)
+		
 
 if __name__ == '__main__':
     try:
@@ -108,7 +145,7 @@ if __name__ == '__main__':
 
     	apixu = Apixu()
     	apixu._request('Madrid')
-    	apixu._get_info('1', 'basic')
+    	apixu._get_info('1', 'advanced, s')
 
     except rospy.ROSInterruptException:
         pass
