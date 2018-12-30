870c108ded3340d64ddad057d9b0b8a7850de7e7
sergio-VM_16_04_3-32bits <sergigon@ing.uc3m.es>
2018-12-30 02:46:58 +0100

    Complete redesign of the class, added weather_format_changer functionality, new URL request format, saving json added extra info functionality (in progress)

--- include/time_weather_skill/weatherClass.py
+++ include/time_weather_skill/weatherClass.py
@@ -15,7 +15,7 @@ import rospy
 import requests # URL requests
 
 from time_weather_skill.create_json import CreateJson
-from time_weather_skill.csv_reader import csv_reader_changer
+from time_weather_skill.weather_format_changer import source2standard
 
 pkg_name = 'time_weather_skill'
 
@@ -36,29 +36,14 @@ class Weather():
         self._SOURCE_LIST = ['apixu', 'source1', 'source2'] # List of the sources
         self._create_json = CreateJson() # CreateJson object
 
-    def _format_change(self, source, json_data):
-        """
-        Changes the weather dictionary format into the STANDARD format.
-        
-        @param source: Name of the source to make the request.
-        @param json_data: Input weather dicionary.
-
-        @return standard_weather_dic: Standard output weather dictionary.
-        """
-
-        info_output = csv_reader_changer(filename, source, json_data)
-        print("info_output: '" + info_output + "'")
-
-        pass
-
-
-    def _save_json(self, json_info):
+    def _save_json(self, json_info, extra_dic):
         """
         Save Json data in a Json file. It adds extra information.
 
         IMPORTANT: Json data must be in STANDARD format.
 
         @param json_info: Weather dictionary in STANDARD format.
+        @param extra_dic: Dictionary with extra info to save.
         """
         """
         data_folder = '' # Folder to store the json files
@@ -69,38 +54,20 @@ class Weather():
         year, month, day = date_now[0].split("-") # Separate date
         """
         # Add extra info
-        json_info.update({'lang': 'es'})
-        #json_info.update({'day': day})
-        #json_info.update({'month': month})
-        #json_info.update({'year': year})
+        json_info.update(extra_dic)
  
         # Write the data in the JSON file
         self._create_json.write(json_info, 'prueba_json') # Write weather info into JSON file
 
-
-
-    def _URL_request(self, source, location):
+    def _URL_request(self, url_forecast, params):
         """
         URL request method.
 
-        @param source: Name of the source to make the request.
-        @param location: City to get the weather. Format: "Madrid" or "Madrid, Spain".
+        @param params: Dictionary with the URL equest params.
         
         @return result: Final result.
         @return result_info_dic: Weather dictionary result.
         """
-
-        # Update URL and parameters to make the URL request
-        url_forecast = ''
-        params = {}
-        if(source == 'apixu'): # Apixu source
-            url_forecast = 'http://api.apixu.com/v1/forecast.json'
-            params = {
-                'key': '9838870ce6324daf95d161656180811',
-                'q': location,
-                'days': 7, # Forecast range
-                'lang': 'es'
-        }
 
         # Make URL request
         try:
@@ -117,11 +84,12 @@ class Weather():
             print('Connection ERROR')
             return -1, {}
 
-    def _get_weather(self, city_name, forecast, date, info_required):
+    def _get_weather(self, location, forecast_type, date, info_required):
         """
         Get weather class.
 
-        @param city: City to get weather.
+        @param location: City to get weather. Format: 'Madrid' or 'Madrid, Spain'.
+        @param forecast_type: 'forecast' or 'current'
         @param date: Date for the weather.
         @param info_required: Type of info_required needed.
 
@@ -129,21 +97,49 @@ class Weather():
         @return result_info_dic: Weather dictionary result.
         """
 
+        # Initialize reulsts
         result, result_info_dic = -1, {}
+        lang = 'es'
 
-        # Checks if it is in local
+        ############### Checks if it is in local #################
         '''
         if True:
             return 0, result_info_dic
         '''
 
-        # Make URL requests
-        for source in self._SOURCE_LIST:
-            result, result_info_dic = self._URL_request(source, city_name)
-            if (result == 0): # URL request success
-                #result_info_dic = self._format_change(source, result_info_dic) # Change weather format
-                self._save_json(result_info_dic) # Saves the content in a local file
-                break # Stops the URL requests
+        ################## Make URL requests #####################
+        # >> Each source must get the FORECAST and CURRENT json <<
+        # >> info and SAVE it in the 'result_info_dic' variable <<
+        for source in self._SOURCE_LIST: # Selects the source from the source list
+            print("Making URL request to: '" + source + "'")
+            ####### Apixu source #######
+            if(source == 'apixu'):
+                # Params
+                url_forecast = 'http://api.apixu.com/v1/forecast.json'
+                params = {
+                    'key': '9838870ce6324daf95d161656180811',
+                    'q': location,
+                    'days': 7, # Forecast range
+                    'lang': lang
+                    }
+                # Request
+                result, result_info_dic = self._URL_request(url_forecast, params)
+
+            ####### Source 2 #######
+            elif(source == 'source2'):
+                #Params
+                #Request
+                pass
+
+            ####### Request success #######
+            if(result == 0): # URL request success
+                result_info_dic = source2standard(source, result_info_dic) # Change weather format to standard format
+                extra_dic = { # Saves extra info in the file
+                    'lang': lang
+                }
+                self._save_json(result_info_dic, extra_dic) # Saves the content in a local file
+                break # Stops the URL requests list
+
 
         return result, result_info_dic
 
@@ -151,7 +147,7 @@ class Weather():
         """
         Manager of the weather class. It updates the result and result_info.
         Examples:
-        madrid/forecast/tomorrow/basic/101
+        madrid/forecast_type/tomorrow/basic/101
         sydney/current/0/is_day/011
 
         @param goal_vec: Vector of the goal.
@@ -163,12 +159,12 @@ class Weather():
         if(len(goal_vec) >= self._GOAL_MAX_SIZE-1): # Check if all min fields are completed
 
             ############## Check weather ################
-            self._city_name = goal_vec[0] # City name
-            self._forecast = goal_vec[1] # Forecast or current info
-            self._date = goal_vec[2] # Date
-            self._info_required = goal_vec[3] # Info wanted
+            location = goal_vec[0] # City name
+            forecast_type = goal_vec[1] # Forecast or current info
+            date = goal_vec[2] # Date
+            info_required = goal_vec[3] # Info wanted
 
-            result, result_info_dic = self._get_weather(self._city_name, self._forecast, self._date, self._info_required) # Get the weather info
+            result, result_info_dic = self._get_weather(location, forecast_type, date, info_required) # Get the weather info
             return result, result_info_dic
 
         else:
